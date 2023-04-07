9861eed31a0864c9a4016b17cb58071943388e26
biancavell <biancavell@hotmail.com>
2023-04-06 22:47:49 -0300

    fix: removed unnecessary if statement on main.py module.

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from management.product_handler import get_products_by_type
 from management.product_handler import add_product
 from management.tab_handler import calculate_tab
 
-
 if __name__ == "__main__":
-    if __name__ == "__main__":
-        table_1 = [
-            {"_id": 1, "amount": 5},
-            {"_id": 19, "amount": 5}
-        ]
-        table_2 = [
-            {"_id": 10},
-            {"_id": 20, "amount": 2},
-            {"_id": 21, "amount": 5},
-        ]
+    table_1 = [
+        {"_id": 1, "amount": 5},
+        {"_id": 19, "amount": 5}
+    ]
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
 
-        print(calculate_tab(table_1))
-        print(calculate_tab(table_2))
+    print(calculate_tab(table_1))
+    print(calculate_tab(table_2))
